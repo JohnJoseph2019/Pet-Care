a0ea8a22d650e7fc5128af3374fc0223d83a8cc7
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-09 17:06:08 -0400

    Dumb rubocup changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { minimum: 6 }  
+  validates :password, length: { minimum: 6 }
 end
