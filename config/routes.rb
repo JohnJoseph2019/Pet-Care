df6cdd13712aa68dce70a60b4764bf2be8be6910
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-09 19:40:28 -0400

    Added a nested resource so I can get User_id an assigne it: each pet belongs to a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
-  resources :users
-  resources :pets
+
+  resources :users do
+    resources :pets
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
