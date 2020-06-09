780d7a61e69d46d60c36056e5c06e4abe11d036a
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-09 18:40:09 -0400

    Change my test code to comment code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  # before_action :authorize_request, except: [:index, :show]
 
   # GET /users
   def index
