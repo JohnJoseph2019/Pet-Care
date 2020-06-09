4ed3b3d37fec790ebc124c72017c56c4d130ab81
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-09 17:43:13 -0400

    Added routes for auth/lgin and verify to use it when needed it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
