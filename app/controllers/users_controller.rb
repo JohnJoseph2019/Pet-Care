c02634dd45b6eb2a285a803c055b7a4414883293
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-09 17:42:03 -0400

    got rid of username because i do not need username in my token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      @token = encode({ user_id: @user.id, username: @user.username })
+      @token = encode({ user_id: @user.id })
       render json: { user: @user, token: @token }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
