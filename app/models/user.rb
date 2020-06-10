4248b48ac95a2faa5a8127b34be9d08870bddd7b
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-10 12:46:25 -0400

    spaces-RuboCop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_secure_password
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
