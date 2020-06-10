38a1c8659fa8b78d5dc932f0b585efff225ef14d
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-10 14:01:29 -0400

    added create and destroy actions in my appointment Controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: :destroy
   # GET pets/:pet_id/appointments
   def index
     @appointments = Appointment.where(pet_id: params[:pet_id])
@@ -8,20 +9,31 @@ class AppointmentsController < ApplicationController
   end
 
   # POST pets/:pet_id/appointments
-  def create; end
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.pet = Pet.find(params[:pet_id])
+
+    if @appointment.save
+      render json: @appointment, status: :created
+    else
+      render json: @appointment.errors, status: :unprocessable_entity
+    end
+  end
 
   # DELETE pets/:pet_id/appointments/1
-  def destroy; end
+  def destroy
+    @appointment.destroy
+  end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_pet
-    @pet = @current_user.pets.find(params[:id])
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
-  def pet_params
-    params.require(:pet).permit(:name, :domestice_type, :breed, :age, :img_url)
+  def appointment_params
+    params.require(:appointment).permit(:restriction_note, :accepted, :start_date, :end_date)
   end
 end
--- app/models/appoinment.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Appointment < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :pet
-end
