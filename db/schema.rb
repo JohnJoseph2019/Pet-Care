7fed46a543d0409ca7fdd9521f29653c58102b64
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-10 16:42:05 -0400

    Basically changin pet_id to null can be true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_131307) do
+ActiveRecord::Schema.define(version: 2020_06_10_193332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_131307) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.bigint "user_id"
-    t.bigint "pet_id", null: false
+    t.bigint "pet_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["pet_id"], name: "index_appointments_on_pet_id"
