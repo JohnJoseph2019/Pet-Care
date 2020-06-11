87ae74e866c9ecb3681a39f686d7d32573214e9d
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-11 12:57:24 -0400

    Here added my state variable for when login in now I also wrote a handle change and handle submit so I can set state and also be able to update state via App js is succesfully logged in

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,38 +1,58 @@
-import React, { Component } from 'react'
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
 
 export default class Login extends Component {
   state = {
+    username: "",
+    password: "",
+  };
 
-  }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleLoginSubmit(this.state);
+  };
+
   render() {
-    const { username, password } = this.state
+    const {username, password} = this.state;
     return (
-<form onSubmit={this.onSignIn}>
-            <div className="userInput">
-              <input
-                required
-                type="text"
-                name="username"
-                value={username}
-                id="user-input"
-                placeholder="username"
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="passwordInput">
-              <input
-                required
-                type="password"
-                name="password"
-                value={password}
-                id="password-input"
-                placeholder="Password (min 6 characters)"
-                onChange={this.handleChange}
-              />
-            </div>
-            {this.renderError()}
-          </form>
-
-    )
+      <>
+        <div className='Login-Title'>Login</div>
+        <form onSubmit={this.handleSubmit}>
+          <label className='usernameInput' htmlFor='username'>
+            Username:
+            <input
+              required
+              type='text'
+              name='username'
+              value={username}
+              id='username-input'
+              placeholder='Username...'
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <label className='passwordInput' htmlFor='username'>
+            Password:
+            <input
+              required
+              type='password'
+              name='password'
+              value={password}
+              id='password-input'
+              placeholder='Password...'
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <Link>Register</Link>
+          <button>Submit</button>
+        </form>
+      </>
+    );
   }
 }
