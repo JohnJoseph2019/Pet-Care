267e937128610de73b99e070a6489827c8de213a
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-11 12:58:13 -0400

    formattin

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-      <div className="header title">PetCare</div>
-      <Link to="/user/login">Login/Register</Link>
+      <div className='header title'>PetCare</div>
+      <Link to='/user/login'>Login/Register</Link>
+      <hr />
     </div>
-  )
+  );
 }
