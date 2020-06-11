c73c2d49679eab90e44ee04b5f5966298a49fe12
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-11 01:29:07 -0400

    Added pets.js for api calls for pets

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,5 +1,5 @@
   
-import api from './api-helper';
+import api from './apiConfig';
 
 export const loginUser = async (loginData) => {
   const resp = await api.post('/auth/login', { auth: loginData })
