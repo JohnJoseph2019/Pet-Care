aa2753dbd9073b34b708e8eaaf6b75c190ad1d33
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-11 12:55:57 -0400

    Added my Login component to render in Main

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,17 +1,20 @@
-import React, { Component } from 'react'
-import {Route} from 'react-router-dom'
+import React, {Component} from "react";
+import {Route} from "react-router-dom";
+import Login from "./Login";
 
 export default class Main extends Component {
   render() {
     return (
-      <div  className="main-div">
-        <Route path='/user/login' render={() => (
-          <>
-            <h2>Login</h2>
-          </>
-        )}
+      <div className='main-div'>
+        <Route
+          path='/user/login'
+          render={() => (
+            <>
+              <Login handleLoginSubmit={this.props.handleLoginSubmit} />
+            </>
+          )}
         />
       </div>
-    )
+    );
   }
 }
