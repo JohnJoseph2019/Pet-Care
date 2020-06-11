9ba559f5f59a385d3139edb335612ae63034a2f2
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-11 12:59:03 -0400

    Added ny state for current user and a async function to check if the credential are good for login in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
-import React, { Component } from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
-
+import React, {Component} from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import {loginUser} from "./services/auth";
 
 export default class App extends Component {
+  state = {
+    currentUser: null,
+  };
+  handleLoginSubmit = async (loginData) => {
+    const currentUser = await loginUser(loginData);
+    this.setState({currentUser});
+  };
   render() {
     return (
       <>
         <Header />
-        <Main/>
+        <Main handleLoginSubmit={this.handleLoginSubmit} />
       </>
-    )
+    );
   }
 }
-
