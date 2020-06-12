92eb249069aa627dbc7b2f4c07e0aa531a1e110e
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-12 15:34:01 -0400

    added api-helper

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,9 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = 'http://localhost:3000'
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://petcare-backend-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
-  baseURL: baseUrl
-})
+  baseURL: baseUrl,
+});
 
 export default api;
