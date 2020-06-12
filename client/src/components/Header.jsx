d8b32005abe50a861a96c483be45b9e233a51819
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-11 20:42:17 -0400

    Adding styling to header and created a heder css file

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header(props) {
   console.log(props.currenUser);
   return (
-    <div>
-      <div className='header title'>PetCare</div>
-      {props.currentUser ? (
+    <div className='header-outDiv'>
+      <div className='headerTitle'>PET CARE</div>
+      {/* {props.currentUser ? (
         <>
           {props.currentUser.username}
           <button onClick={props.handleLogout}>Log Out</button>
@@ -14,7 +15,7 @@ export default function Header(props) {
       ) : (
         <Link to='/user/login'>Login/Register</Link>
       )}
-      <hr />
+      <hr /> */}
     </div>
   );
 }
