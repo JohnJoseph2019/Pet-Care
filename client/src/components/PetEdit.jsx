8d3d24c5de2db9251265556591d4b53539612b71
Juan Delgado <Delgado-juan@hotmail.com>
2020-06-14 00:12:41 -0400

    Completed the funcionality of PetDetail and PetEdit

--- client/src/components/PetEdit.jsx
+++ client/src/components/PetEdit.jsx
@@ -4,23 +4,24 @@ import './PetEdit.css';
 export default function PetEdit(props) {
   console.log('aaaaaaa', props.petData);
   const { name, breed, pet_type, age, img_url } = props.petData;
-
   return (
     <div>
       <>
+        <div className='EditTitle'>Edit Pet</div>
         <form
           className='petEditForm'
           onSubmit={e => {
             e.preventDefault();
             props.editSubmit(props.petId);
+            props.history.push(`/pets/${props.petId}`);
           }}>
-          <div className='petEditImg'>
-            <img id='img' className='petDetailImage' src={img_url} alt={pet_type} />
-            <label className='LabelDetailLabel' htmlFor='img_url'>
+          <div className='petOutertImg'>
+            <img id='img' className='petEditImage' src={img_url} alt={pet_type} />
+            <label className='petEditLabel' htmlFor='img_url'>
               Image Link:
               <input
                 id='img_url'
-                className='LabelDetail'
+                className='petInput'
                 type='text'
                 name='img_url'
                 value={img_url}
@@ -29,44 +30,44 @@ export default function PetEdit(props) {
             </label>
           </div>
           <div className='petInfoDetail'>
-            <label className='LabelDetailLabel' htmlFor='name'>
+            <label className='petEditLabel' htmlFor='name'>
               Name:
               <input
                 id='name'
-                className='LabelDetail'
+                className='petInput'
                 type='text'
                 name='name'
                 value={name}
                 onChange={props.handleChange}
               />
             </label>
-            <label className='LabelDetailLabel' htmlFor='age'>
+            <label className='petEditLabel' htmlFor='age'>
               Age:
               <input
                 id='age'
-                className='LabelDetail'
+                className='petInput'
                 type='number'
                 name='age'
                 value={age}
                 onChange={props.handleChange}
               />
             </label>
-            <label className='LabelDetailLabel' htmlFor='breed'>
+            <label className='petEditLabel' htmlFor='breed'>
               Breed:
               <input
                 id='breed'
-                className='LabelDetail'
+                className='petInput'
                 type='text'
                 name='breed'
                 value={breed}
                 onChange={props.handleChange}
               />
             </label>
-            <label className='LabelDetailLabel' htmlFor='pet_type'>
+            <label className='petEditLabel' htmlFor='pet_type'>
               Type:
               <input
                 id='pet_type'
-                className='LabelDetail'
+                className='petInput'
                 type='text'
                 name='pet_type'
                 value={pet_type}
